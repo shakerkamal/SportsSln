191a894654b548c04af6b64b882a94f870b57794
shakerkamal <shakerkamal18@gmail.com>
2023-11-22 20:23:02 +0100

    Added product details page and create, update and delete features

--- SportsStore/Implementations/StoreRepository.cs
+++ SportsStore/Implementations/StoreRepository.cs
@@ -13,5 +13,20 @@ namespace SportsStore.Implementations
         }
 
         public IQueryable<Product> Products => _dbContext.Products;
+
+        public void CreateProduct(Product p)
+        {
+            _dbContext.Add(p);
+            _dbContext.SaveChanges();
+        }
+        public void DeleteProduct(Product p)
+        {
+            _dbContext.Remove(p);
+            _dbContext.SaveChanges();
+        }
+        public void SaveProduct(Product p)
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
